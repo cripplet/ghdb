80369b37dbd786d1e46475ac3ee8ac271e4079bc
Minke Zhang <minke.zhang@gmail.com>
2016-04-28 04:06:05 -0700

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -8,25 +8,3 @@ var email = "minke.zhang@gmail.com";
 var username = "cripplet-db";
 var password = "6f920f50f5c81308ee0" + "aad463a40ac83b4cd5b16";
 var repo = "db";
-
-
-/**
- * vars = { "name": NAME, ... }
- */
-var configure = function(vars) {
-  if (vars.hasOwnProperty("name")) {
-    name = vars.name;
-  }
-  if (vars.hasOwnProperty("email")) {
-    email = vars.email;
-  }
-  if (vars.hasOwnProperty("username")) {
-    username = vars.username;
-  }
-  if (vars.hasOwnProperty("password")) {
-    password = vars.password;
-  }
-  if (vars.hasOwnProperty("repo")) {
-    repo = vars.repo;
-  }
-}
