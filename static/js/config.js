0a759dd8347938d192af03eb84eab59393b2cfab
Minke Zhang <minke.zhang@gmail.com>
2016-04-28 03:07:31 -0700

    Create config.js

---
+++ static/js/config.js
@@ -0,0 +1,10 @@
+/**
+ * Params file for lib.js.
+ */
+
+export const name = "Minke Zhang";
+export const email = "minke.zhang@gmail.com";
+
+export const username = "cripplet-db";
+export const password = "6f920f50f5c81308ee0" + "aad463a40ac83b4cd5b16";
+export const repo = "db";
