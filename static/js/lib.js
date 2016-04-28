17d74fd8d1402af378345078f2c3a2a74b085220
Minke Zhang <minke.zhang@gmail.com>
2016-04-28 00:56:30 -0700

    Update lib.js

--- static/js/lib.js
+++ static/js/lib.js
@@ -18,26 +18,42 @@ var getMockPath = function() {
   return (Math.random().toString(36)+'00000000000000000').slice(2, 16+2)
 };
 
-var getFile = function(path, el_status, el_debug, el_data) {
+var getFile = function(path, el_status = null, el_debug = null, el_data = null) {
+  var sha = null;
+
   $.ajax({
     type: "GET",
     url: getEndpoint(path),
     dataType: "json",
     contentType: "application/json",
     success: function(resp) {
-      el_status.text("success");
-      el_debug.text(JSON.stringify(resp));
-      el_data.text(atob(resp.content));
+      if (el_status !== null) {
+        el_status.text("success");
+      }
+      if (el_debug !== null) {
+        el_debug.text(JSON.stringify(resp));
+      }
+      if (el_data !== null) {
+        el_data.text(atob(resp.content));
+      }
+      sha = resp.sha;
     },
     error: function(req) {
-      el_status.text("failure");
-      el_debug.text(JSON.stringify(req));
+      if (el_status !== null) {
+        el_status.text("failure");
+      }
+      if (el_debug !== null) {
+        el_debug.text(JSON.stringify(req));
+      }
     }
   });
+
+  return sha;
 }
 
 var addFile = function(path, content, el_status, el_debug, el_data) {
   path = getMockPath();
+
   var payload = {
     "path": path,
     "message": "added " + path,
@@ -57,13 +73,23 @@ var addFile = function(path, content, el_status, el_debug, el_data) {
     headers: { 'Authorization': "Basic " + btoa(username + ":" + password) },
     data: JSON.stringify(payload),
     success: function(resp) {
-      el_status.text("success");
-      el_debug.text(JSON.stringify(resp));
-      el_data.text(resp.content.name);
+      if (el_status !== null) {
+        el_status.text("success");
+      }
+      if (el_debug !== null) {
+        el_debug.text(JSON.stringify(resp));
+      }
+      if (el_data !== null) {
+        el_data.text(resp.content.name);
+      }
     },
     error: function(req) {
-      el_status.text("failure");
-      el_debug.text(JSON.stringify(req));
+      if (el_status !== null) {
+        el_status.text("failure");
+      }
+      if (el_debug !== null) {
+        el_debug.text(JSON.stringify(req));
+      }
     }
   });
 };
