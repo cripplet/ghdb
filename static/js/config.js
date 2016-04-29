341a86ce60581327fb0809b239e6162f12ba25b4
Minke Zhang <minke.zhang@gmail.com>
2016-04-28 17:00:59 -0700

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -6,7 +6,7 @@ var config = {
   "name": "Minke Zhang",
   "email": "minke.zhang@gmail.com",
 
-  "username" = "cripplet-db",
-  "password" = "6f920f50f5c81308ee0" + "aad463a40ac83b4cd5b16",
-  "repo" = "db"
+  "username": "cripplet-db",
+  "password": "6f920f50f5c81308ee0" + "aad463a40ac83b4cd5b16",
+  "repo": "db"
 }
